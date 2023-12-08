83adc16c9545472091bbc15cff3d23fc402ab591
Hector Daniel Gonzalez Laport <danigl_1891@hotmail.com>
2023-12-08 18:26:25 -0300

    Añadido el Buscador

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Pokemon from './Components/Pokemon';
 
-
-const PokemonList = () => {
+const App = () => {
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=150';
   const [pokemonList, setPokemonList] = useState([]);
+  const [filteredPokemonList, setFilteredPokemonList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,6 +21,7 @@ const PokemonList = () => {
 
         const detailedPokemonData = await Promise.all(promises);
         setPokemonList(detailedPokemonData);
+        setFilteredPokemonList(detailedPokemonData);
       } catch (error) {
         console.error('Error al obtener datos de la API:', error);
       }
@@ -28,16 +30,35 @@ const PokemonList = () => {
     fetchData();
   }, []); // La dependencia vacía asegura que useEffect se ejecute solo una vez al montar el componente
 
+  const handleInputChange = (event) => {
+    const newValue = event.target.value;
+    setSearchTerm(newValue);
+
+    // Filtrar la lista de Pokémon según el término de búsqueda
+    const filteredList = pokemonList.filter((pokemon) => {
+      const name = pokemon.name.toLowerCase();
+      return name.includes(newValue.toLowerCase());
+    });
+
+    setFilteredPokemonList(filteredList);
+  };
+
   return (
     <div>
       <h1>Lista de Pokémon</h1>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="Buscar por nombre o ID..."
+      />
       <ul>
-        {pokemonList.map((pokemon) => (
-          <Pokemon pokemon = {pokemon} />
+        {filteredPokemonList.map((pokemon) => (
+          <Pokemon pokemon={pokemon} key={pokemon.id} />
         ))}
       </ul>
     </div>
   );
 };
 
-export default PokemonList;
+export default App;
